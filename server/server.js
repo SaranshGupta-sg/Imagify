601e60c62ddf36ea11e53bbf4e398592f4eae118
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-07-29 00:10:29 +0530

    new

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import userRouter from './routes/userRoutes.js'
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -9,7 +14,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+await connectDB();
 
-app.get('/', (req, res) => res.send("API Working"))
+app.get("/", (req, res) => res.send("API Working"));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// ROUTES
+app.use('/api/user', userRouter)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
