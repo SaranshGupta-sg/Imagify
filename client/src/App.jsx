de81ba91e01da77de19bcf76e1c3e7be1633404c
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-07-30 18:07:16 +0530

    new

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,55 @@
-import { useContext } from 'react'
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import BuyCredit from "./pages/BuyCredit";
 import Result from "./pages/Result";
 import Navbar from "./components/Navbar";
-import Footer from './components/Footer'
-import Login from './components/Login'
-import { AppContext } from './context/AppContext'
-
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import { AppContext } from "./context/AppContext";
 
 const App = () => {
-  const { showLogin } = useContext(AppContext)
+  const { showLogin } = useContext(AppContext);
+  const location = useLocation();
+
   return (
-    <div className="px-4 sm:px10 md:px-14 lg:px-28 min-h-screen bg-linear-to-b from-teal-50 to-orange-50">
-      <Navbar />
-       {showLogin && <Login />}
+    <div className="min-h-screen bg-linear-to-b from-violet-50 via-white to-teal-50">
+      <div className="px-4 sm:px-10 md:px-14 lg:px-28">
+        <Navbar />
+      </div>
+
+      {showLogin && <Login />}
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/buy" element={<BuyCredit />} />
+        <Route
+          path="/"
+          element={
+            <div className="px-4 sm:px-10 md:px-14 lg:px-28">
+              <Home />
+            </div>
+          }
+        />
+        <Route
+          path="/result"
+          element={
+            <div className="px-4 sm:px-10 md:px-14 lg:px-28">
+              <Result />
+            </div>
+          }
+        />
+        <Route
+          path="/buy"
+          element={
+            <div className="px-4 sm:px-10 md:px-14 lg:px-28">
+              <BuyCredit />
+            </div>
+          }
+        />
       </Routes>
-      <Footer />
+
+      <div className="px-4 sm:px-10 md:px-14 lg:px-28">
+        <Footer />
+      </div>
     </div>
   );
 };
